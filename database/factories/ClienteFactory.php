f6cbbd113018b271df4fcf447e2f67c1eb34b17b
jelt0024 <jelt0024@gmail.com>
2022-08-02 12:08:49 -0600

    Agregue Archivos

---
+++ database/factories/ClienteFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ClienteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nombre' => $this->faker->name(),
+            'direccion' => $this->faker->text(),
+            'telefono' => $this->faker->phoneNumber()
+        ];
+    }
+}
---
+++ database/seeders/ClientesSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Cliente;
+
+class ClientesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Cliente::factory()
+            ->count(500)
+            ->create();
+    }
+}
