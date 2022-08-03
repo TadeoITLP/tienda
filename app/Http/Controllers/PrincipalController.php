1721ec3dea8d5a953e2db795ae8cda312d7527fe
TadeoITLP <tadeo.rs@lapaz.tecnm.mx>
2022-08-03 11:39:51 -0600

    Relaciones y Middleware

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cliente;
 use Illuminate\Http\Request;
 use App\Models\Producto;
+use Illuminate\Support\Facades\Hash;
+use Session;
+
 
 class PrincipalController extends Controller
 {
@@ -15,5 +19,40 @@ class PrincipalController extends Controller
         //return $productos;
         return view("principal",["productosNuevos"=>$productos]);
                                 //*,"productosMasVendidos"=>$productosMasVendidos]);
+    }
+
+    public function guardarProducto(Request $request)
+    {
+        $producto = new Producto();
+        $producto->nombre = $request->nombre;
+        $producto->descripcion = $request->descripcion;
+        $producto->precio = $request->precio;
+        $producto->cantidad = $request->cantidad;
+        $producto->foto = "img\galleta_animalitos.jpg";
+        $producto->save();
+        redirect()->back();
+    }
+
+    public function login(Request $request)
+    {
+        //echo "Nombre".$request->usuario;
+        $cliente = Cliente::where('nombre',$request->usuario)->get()->first();
+        if ($cliente)
+        {
+            if ( Hash::check($request->password, $cliente->password))
+            {
+                Session::put("cliente",$cliente);
+                //echo "Ok";
+                route("inicio");
+            }
+            else{
+                //echo "Error";
+                return back()->with('error', 'No usuario o contraseña incorrectos!');
+            }
+        }
+        else
+        {
+            //echo "Error cliente no existe";
+        }
     }
 }
