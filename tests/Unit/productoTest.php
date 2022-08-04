5ea02fde11499adc4388bf4b1506fa8bf3a65df5
jelt0024 <jelt0024@gmail.com>
2022-08-04 12:38:09 -0600

    Cambios

--- tests/Unit/productoTest.php
+++ tests/Unit/productoTest.php
@@ -4,23 +4,28 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use App\Models\Producto;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class productoTest extends TestCase
+class ProductoTest extends TestCase
 {
+    use DatabaseTransactions;
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function test_masVendidos()
-    {
-        $this->assertTrue(true);
-    }
     public function test_masNuevos()
     {
         $productos = Producto::masNuevos();
         $this->assertTrue($productos->count()==4);
     }
+
+    public function test_masVendidos()
+    {
+        $productos = Producto::masVendidos();
+        $this->assertTrue($productos->count()<=4);
+    }
+
     public function test_porCategoria()
     {
         $this->assertTrue(true);
